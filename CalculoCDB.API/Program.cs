0fccfe27f4e3944acffe2bb64105dba8db077f80
Guilherme Lopes Sathler <guilhermesat15@gmail.com>
2025-01-28 16:42:52 -0300

    Atualização política CORS

--- CalculoCDB.API/Program.cs
+++ CalculoCDB.API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "MyPolicy",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("*")
                 .AllowAnyMethod()
                 .AllowAnyHeader();
         });
